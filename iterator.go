3e0e3d9e65bbc2a8b698132192bb85ceb5c0576d
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-05-26 14:40:57 +0100

    Restructured iterator knocks for simplicity

--- iterator.go
+++ iterator.go
@@ -5,29 +5,15 @@ import (
 )
 
 func iterate(settings *Settings, config *Config) {
-
-  shiftRotorTwo := false
-  shiftRotorThree := false
-
-  if letterMaths(settings.Rotors[0].Position, 1, config) == config.Rotors[settings.Rotors[0].Type].Step {
-    shiftRotorTwo = true
-
-    if letterMaths(settings.Rotors[1].Position, 1, config) == config.Rotors[settings.Rotors[1].Type].Step {
-      shiftRotorThree = true
-    }
-  }
-
   // Shift rotor 1
   settings.Rotors[0].Position = incrementLetter(settings.Rotors[0].Position, config)
-
-  // Shift rotor 2
-  if shiftRotorTwo  {
+  if settings.Rotors[0].Position == config.Rotors[settings.Rotors[0].Type].Step {
+    // Shift rotor 2
     settings.Rotors[1].Position = incrementLetter(settings.Rotors[1].Position, config)
-  }
-
-  // Shift rotor 3
-  if shiftRotorThree {
-    settings.Rotors[2].Position = incrementLetter(settings.Rotors[2].Position, config)
+    if settings.Rotors[1].Position == config.Rotors[settings.Rotors[1].Type].Step {
+      // Shift rotor 3
+      settings.Rotors[2].Position = incrementLetter(settings.Rotors[2].Position, config)
+    }
   }
 }
 
